c18f45996f961b2698e8f1536c6d1eeb5165cd74
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2020-09-14 13:18:28 +0200

    WIP: resources test.

---
+++ tests/resources/test_resources.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
+#
+# Invenio-Records-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Service tests."""
+
+
+def test_search(app, client):
+    """."""
+    res = client.get('/mocks')
+    print(res)
